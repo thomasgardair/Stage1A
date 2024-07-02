b65361ba90b97a71f80e2d30685d4f5c8a63d5d4
tgardair <thomas.gardair@insee.fr>
2024-07-02 14:27:50 +0000

    Update

---
+++ fonctions/Wilcoxon.R
@@ -0,0 +1,11 @@
+wilcoxon_test <- function(tableau_complet, nb_obs, nb_obs_pert, paired = FALSE) {
+  
+  obs <- tableau_complet[[nb_obs]]
+  obs_pert <- tableau_complet[[nb_obs_pert]]
+  
+  test <- wilcox.test(obs, obs_pert, paired = paired)
+  
+  return(test)
+}
+wilcoxon_test(tableau_complet, "nb_obs", "nb_obs_pert", paired = TRUE)
+
