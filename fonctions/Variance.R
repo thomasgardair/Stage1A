46e25925a3263063c350b4e8ffa85c4ca6d2ec24
tgardair <thomas.gardair@insee.fr>
2024-07-03 08:15:51 +0000

    Variance

---
+++ fonctions/Variance.R
@@ -0,0 +1,9 @@
+VR <- function(tableau_complet, n_obs,n_obs_pert){
+  obs <- tableau_complet[[n_obs]]
+  obs_pert <- tableau_complet[[n_obs_pert]]
+  V_orig <- mean((obs -(mean(obs)))^2 )
+  V_pert <- mean((obs_pert - (mean(obs)))^2)
+  vr <- 100*(abs(V_orig - V_pert)/V_orig)
+  return(vr)
+}
+
