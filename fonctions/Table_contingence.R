d2ec3cd12796944744ea0814508e3214bccadf16
tgardair <thomas.gardair@insee.fr>
2024-07-04 09:45:42 +0000

    Update

--- fonctions/Table_contingence.R
+++ fonctions/Table_contingence.R
@@ -20,3 +20,27 @@ for (vars in combinaisons) {
   assign(table_pert, table_obs_pert, envir = .GlobalEnv)
 }
 
+sous_tableau <- function(tableau_complet) {
+  tab_sans_marges <- tableau_complet %>% 
+    filter(across(everything(), ~ . != "Total"))
+  
+  columns <- colnames(tab_sans_marges)
+  columns <- columns[grepl("^[A-Z_]+$", columns)]
+  
+  combinations <- combn(columns, 2, simplify = FALSE)
+  
+  for (vars in combinations) {
+    formula_obs <- as.formula(paste("nb_obs ~", paste(vars, collapse = " + ")))
+    formula_obs_pert <- as.formula(paste("nb_obs_pert ~", paste(vars, collapse = " + ")))
+    
+    table_obs <- xtabs(formula_obs, data = tab_sans_marges)
+    table_obs_pert <- xtabs(formula_obs_pert, data = tab_sans_marges)
+    
+    table_orig <- paste(paste(vars, collapse = "_x_"), "orig", sep = "_")
+    table_pert <- paste(paste(vars, collapse = "_x_"), "pert", sep = "_")
+    
+    assign(table_orig, table_obs, envir = .GlobalEnv)
+    assign(table_pert, table_obs_pert, envir = .GlobalEnv)
+  
+  }
+}
