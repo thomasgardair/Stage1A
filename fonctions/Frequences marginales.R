9ece9f7974b8aaa36833c0f8436c32c269d3d7a3
tgardair <thomas.gardair@insee.fr>
2024-07-04 13:49:00 +0000

    Update

--- fonctions/Frequences marginales.R
+++ fonctions/Frequences marginales.R
@@ -1,8 +1,27 @@
 library(dplyr)
 library(data.table)
-
+library(rlang)
 freq_marg_sex <- tableau_complet %>%
   filter(DIPL == "Total" & AGE == "Total" & REGION == "Total") %>%
-  select(SEX, nb_obs)
+  select(SEX, nb_obs, nb_obs_pert)
 
 freq_marg_sex
+
+frequences_marginales <- function(tableau_complet, var){
+  freq <- tableau_complet %>% 
+    filter(across(everything(), ~ . == "Total")) %>%
+    select(var, nb_obs, nb_obs_pert)
+  return(freq)
+}
+frequences_marginales(tableau_complet, "SEX")
+
+
+frequences_marginales <- function(tableau_complet, var){
+  freq <- tableau_complet %>%
+    filter(across(-all_of(var), ~ . == "Total")) %>%
+    select(all_of(var), nb_obs, nb_obs_pert)
+  
+  return(freq)
+}
+
+frequences_marginales(tableau_complet, "SEX")
