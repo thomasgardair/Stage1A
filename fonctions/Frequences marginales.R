3548b8f157e10ea9b929c4865fdad55313ee98ea
tgardair <thomas.gardair@insee.fr>
2024-07-04 14:54:02 +0000

    Update

--- fonctions/Frequences marginales.R
+++ fonctions/Frequences marginales.R
@@ -15,13 +15,14 @@ frequences_marginales <- function(tableau_complet, var){
 }
 frequences_marginales(tableau_complet, "SEX")
 
-
 frequences_marginales <- function(tableau_complet, var){
+  columns <- colnames(tableau_complet)
+  columns <- columns[grepl("^[A-Z_]+$", columns)]
+  columns <- setdiff(columns, var)
+  
   freq <- tableau_complet %>%
-    filter(across(-all_of(var), ~ . == "Total")) %>%
+    filter(if_all(all_of(columns), ~ . == "Total")) %>%
     select(all_of(var), nb_obs, nb_obs_pert)
   
   return(freq)
 }
-
-frequences_marginales(tableau_complet, "SEX")
